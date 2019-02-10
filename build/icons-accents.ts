1a78164a110537fd026f06410000fe949606da54
Alessio Occhipinti <LasaleFamine@users.noreply.github.com>
2019-02-10 19:21:48 +0100

    Variant icons fix (#32)
    * chore: better build response
    
    * fix: check on variant theme

--- build/icons-accents.ts
+++ build/icons-accents.ts
@@ -16,7 +16,6 @@ const replaceNameWithAccent = (name: string, accentName: string): string =>
 const replaceSVGColour = (filecontent: string, colour: string): string =>
   filecontent.replace(new RegExp('#(80CBC4)', 'i'), ($0, $1) => {
     const newColour = colour.replace('#', '');
-    console.log(`Replacing colour ${ $1 } with ${ newColour }`);
     return $0.replace($1, newColour);
   });
 
@@ -27,7 +26,6 @@ const writeSVGIcon = (fromFile: string, toFile: string, accent: string): void =>
   const fileContent: string = fs.readFileSync(normalizeIconPath(fromFile), 'utf-8');
   const content: string = replaceSVGColour(fileContent, DEFAULTS.accents[accent]);
   const pathToFile = normalizeIconPath(toFile);
-  console.log(`Accented icon ${pathToFile} created with colour ${ accent } (${ DEFAULTS.accents[accent] })`);
   fs.writeFileSync(pathToFile, content);
 };
 
@@ -37,10 +35,8 @@ export default (): Promise<void> => {
   for (const key of Object.keys(DEFAULTS.accents)) {
     const iconName = replaceWhiteSpaces(key);
     const themecopy: IThemeIconsAccents = JSON.parse(JSON.stringify(basetheme));
-    const variantPath: string = PATHS.pathIconKey(key);
 
     for (const accentableIconName of DEFAULTS.accentableIcons) {
-      console.log(`Preparing ${accentableIconName} accented icon`);
       const iconOriginDefinition: IThemeIconsItem = (basetheme.iconDefinitions as any)[accentableIconName];
       const iconCopyDefinition: IThemeIconsItem = (themecopy.iconDefinitions as any)[accentableIconName];
 
@@ -51,7 +47,6 @@ export default (): Promise<void> => {
         console.log(`Icon ${accentableIconName} not found`);
       }
     }
-    console.log('Accentable icons generated', variantPath);
   }
 
   return Promise.resolve();
